e576623bf3a1b4abfb7b54199628fb0968a1acd3
Emerson Cardoso <hemersonycardoso@gmail.com>
2023-01-18 19:36:23 +0100

    class

---
+++ lib/src/common/common.dart
@@ -0,0 +1,128 @@
+class User {
+  String lastName;
+  int firstName;
+  String email;
+  String password;
+  String username;
+  String role;
+
+  User(
+      {required this.lastName,
+      required this.firstName,
+      required this.email,
+      required this.username,
+      required this.role,
+      required this.password});
+}
+
+class Event {
+  String name;
+  String description;
+  DateTime date;
+
+  Event({required this.name, required this.description, required this.date});
+}
+
+class EventCalendar extends Event {
+  String location;
+
+  EventCalendar(
+      {required super.name,
+      required super.description,
+      required super.date,
+      required this.location});
+}
+
+class Calendar {
+  List<EventCalendar> events;
+
+  Calendar({required this.events});
+
+  void addEvent(EventCalendar event) {
+    events.add(event);
+  }
+
+  void removeEvent(EventCalendar event) {
+    events.remove(event);
+  }
+}
+
+class EventTimelime extends Event {
+  String mediaResoureURL;
+
+  EventTimelime(
+      {required super.name,
+      required super.description,
+      required super.date,
+      required this.mediaResoureURL});
+}
+
+class Timeline {
+  List<EventTimelime> events;
+
+  Timeline({required this.events});
+
+  void addEvent(EventTimelime event) {
+    events.add(event);
+  }
+
+  void removeEvent(EventTimelime event) {
+    events.remove(event);
+  }
+}
+
+class News {
+  String title;
+  String description;
+  String author;
+  String imageurl;
+
+  News(
+      {required this.title,
+      required this.description,
+      required this.author,
+      required this.imageurl});
+
+  factory News.fromJson(Map<String, dynamic> json) {
+    return News(
+      title: json['title'],
+      description: json['description'],
+      author: json['author'],
+      imageurl: json['imageurl'],
+    );
+  }
+}
+
+class Comment {
+  String text;
+  User author;
+  DateTime timestamp;
+
+  Comment({required this.text, required this.author, required this.timestamp});
+}
+
+class Evaluation {
+  int course_organization_score;
+  int course_content_score;
+  int course_teaching_score;
+  int course_general_score;
+
+  Evaluation(
+      {required this.course_organization_score,
+      required this.course_content_score,
+      required this.course_teaching_score,
+      required this.course_general_score});
+}
+
+class Course {
+  String name;
+  String description;
+  List<Comment> comments;
+  List<Evaluation> evaluations;
+
+  Course(
+      {required this.name,
+      required this.description,
+      required this.comments,
+      required this.evaluations});
+}
--- lib/src/feed/feed.dart
+++ lib/src/feed/feed.dart
@@ -1,36 +1,85 @@
 import 'package:flutter/material.dart';
+import 'newsItems.dart';
+import '../common/common.dart';
 
-
+final List<News> newsItems = createNewsList();
 
 class FeedNews extends StatelessWidget {
   const FeedNews({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      top: false,
-      bottom: false,
-
-      child: Padding(
-        padding: const EdgeInsets.all(8), 
-        child: ,
+    return Scaffold(
+      body: ListView(
+        restorationId: "list-news-feed-news",
+        // padding: const EdgeInsets.only(top: 8, left: 8, right: 8),
+        children: [for (final news in newsItems) NewsCard(news: news)],
       ),
-
-      // child: Text("Hello FeedNews!"),
     );
   }
 }
 
-// class FeedNews extends StatelessWidget {
-//   const FeedNews({super.key});
+class NewsCard extends StatelessWidget {
+  const NewsCard({super.key, required this.news});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Theme(
-//       data: ThemeData(
-//           colorScheme:
-//               Theme.of(context).colorScheme.copyWith(primary: Colors.purple)),
-//       child: const Text("Hello FeedNews!"),
-//     );
-//   }
-// }
+  final News news;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.only(top: 15, left: 4, right: 4),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 184,
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                    child: Ink.image(
+                      image: NetworkImage(news.imageurl),
+                      fit: BoxFit.cover,
+                      child: Container(),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 16,
+                    left: 16,
+                    right: 16,
+                    child: FittedBox(
+                      fit: BoxFit.scaleDown,
+                      alignment: Alignment.centerLeft,
+                      child: Text('Author : ${news.author}'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+              child: DefaultTextStyle(
+                softWrap: false,
+                overflow: TextOverflow.ellipsis,
+                style: Theme.of(context).textTheme.subtitle1!,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Titule: ${news.title}',
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                    Text('Description:  ${news.description}',
+                        maxLines: 10,
+                        style: Theme.of(context)
+                            .primaryTextTheme
+                            .caption!
+                            .copyWith(color: Colors.black),
+                        overflow: TextOverflow.ellipsis),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
+}
