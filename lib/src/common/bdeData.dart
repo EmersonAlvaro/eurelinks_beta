ffc64947d9c53e9753edb061aff47ace63814006
sivasank <deepika.sivasankaran@eurecom.fr>
2023-02-02 23:42:53 +0100

    BDE/BDS done, add description for both and the VP name

---
+++ lib/src/common/bdeData.dart
@@ -0,0 +1,23 @@
+import '../model/BDE.dart';
+
+
+BDE bde_members_function(){
+  BDE bde_members = BDE(
+      presidentName: "Robert Moller",
+      vicePresidentName: "BUzzinga",
+      secretaryName: "Clara Leonet",
+      description: "BUzzingaBUzzingaBUzzingaBUzzingaBUzzinga",
+      logoPath: "assets/images/bde.jpg",
+      address: '''
+      BDE EURECOM
+      Campus SophiaTech,
+      450 Route des Chappes, 
+      06410 Biot
+      ''',
+      contact: "+33(0)493008221",
+      email: "bde@eurecom.fr"
+
+  );
+  return bde_members;
+}
+
