a2f749e5854c42ac78c3c248cb50320ff0c95279
sivasank <deepika.sivasankaran@eurecom.fr>
2023-02-02 23:40:58 +0100

    BDE/BDS done, add description for both and the VP name

--- lib/src/bde/bde.dart
+++ lib/src/bde/bde.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import '../bds/edit-bde-bds-view.dart';
+import '../common/bdeData.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class BDEView extends StatefulWidget {
   const BDEView({super.key});
@@ -7,6 +10,8 @@ class BDEView extends StatefulWidget {
   @override
   State<BDEView> createState() => _BDEPageState();
 }
+
+  final bde_team = bde_members_function();
 
 class _BDEPageState extends State<BDEView> {
   @override
@@ -23,38 +28,162 @@ class _BDEPageState extends State<BDEView> {
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        icon: const Icon(Icons.post_add_outlined),
-        label: const Text('edit'),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const EditBDESInfo()),
-          );
-        },
-      ),
+      // floatingActionButton: FloatingActionButton.extended(
+      //   icon: const Icon(Icons.post_add_outlined),
+      //   label: const Text('edit'),
+      //   onPressed: () {
+      //     Navigator.push(
+      //       context,
+      //       MaterialPageRoute(builder: (context) => const EditBDESInfo()),
+      //     );
+      //   },
+      // ),
       body: Column(
         children: [
+        Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset(
+            'assets/images/bde.jpg',
+             height: 150,
+             width: 150),
+          Image.asset(
+            'assets/images/bed_rock_logo.png',
+             height: 150,
+             width: 150),
+        ],
+      ),
           Padding(
-            padding: const EdgeInsets.only(top: 60.0),
-            child: Center(
-              child: SizedBox(
-                width: 200,
-                height: 150,
-                child: Icon(
-                  Icons.person,
-                ),
-                // Image.asset('assets/images/Eurecom.png'),
-              ),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 15),
+            padding: EdgeInsets.only(top: 2.0, bottom: 10.0, left: 5.0, right: 5.0),
             child: Text(
-              "BDE Page ",
+              '${bde_team.description}',
               style: TextStyle(fontSize: 20),
             ),
           ),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+            child: Text(
+              "President: ${bde_team.presidentName}",
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+            child: Text(
+              "Vice President: ${bde_team.vicePresidentName}",
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+            child: Text(
+              "Secretary: ${bde_team.secretaryName}",
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          const SizedBox(height: 30),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: <Color>[
+                            Color(0xFF0D47A1),
+                            Color(0xFF1976D2),
+                            Color(0xFF42A5F5),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.all(16.0),
+                    textStyle: const TextStyle(fontSize: 20),
+                    ),
+                  onPressed: () => launch("tel://${bde_team.contact}"),
+                  child: const Text('Contact Us'),
+                  ),
+                ],
+              ),
+            ),
+          const SizedBox(height: 30),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(4),
+            child: Stack(
+              children: <Widget>[
+                Positioned.fill(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: <Color>[
+                          Color(0xFF0D47A1),
+                          Color(0xFF1976D2),
+                          Color(0xFF42A5F5),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.all(16.0),
+                    textStyle: const TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () async {
+                      await Clipboard.setData(ClipboardData(text: "${bde_team.email}"));
+                      Fluttertoast.showToast(
+                          msg: "Email ID Copied to Clipboard",
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.BOTTOM,
+                          timeInSecForIosWeb: 1,
+                          backgroundColor: Colors.grey,
+                          textColor: Colors.white,
+                          fontSize: 16.0
+                      );
+                      },
+                  child: const Text('Email Us'),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 30),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(4),
+            child: Stack(
+              children: <Widget>[
+                Positioned.fill(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: <Color>[
+                          Color(0xFF0D47A1),
+                          Color(0xFF1976D2),
+                          Color(0xFF42A5F5),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.all(16.0),
+                    textStyle: const TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () async {
+                    await launch('https://www.google.com/maps/place/Eurecom/@43.614386,7.071125,17z/data=!3m1!4b1!4m5!3m4!1s0x12cc2bbceb8ef3b9:0x22dae297f1be6add!8m2!3d43.614386!4d7.071125');
+                  },
+                  child: const Text('Locate Us'),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
